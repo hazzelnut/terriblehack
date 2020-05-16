4f08982743c9316a3ba5d3baa436fd1bbf84c13a
Eric Chan <hazzelnutst@gmail.com>
2020-05-16 15:04:04 -0700

    Update: Add random cache files and update submit form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Some sample text wohoo!'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="formWrapper">
+          <input className="textbox" type="text" value={this.state.value} onChange={this.handleChange} />
+        </div>
+        <button className="submitBtn" onClick={this.handleSubmit}>
+          Button
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
