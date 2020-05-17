3c7c02ceac660b3b73aacc41ec309a1d3a6568b0
Eric Chan <hazzelnutst@gmail.com>
2020-05-16 22:50:10 -0700

    Update app naming

--- api/api.py
+++ api/api.py
@@ -1,13 +1,13 @@
 import time
 from flask import Flask, request
 
-api = Flask(__name__)
+app = Flask(__name__)
 
-@api.route('/time')
+@app.route('/time')
 def get_current_time():
     return {'time': time.time()}
 
-@api.route('/magicwords', methods=['POST'])
+@app.route('/magicwords', methods=['POST'])
 def get_magic_words():
   data = request.get_json()
   value = data['value']
