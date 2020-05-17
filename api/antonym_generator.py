84a94e196d477f3be11181a1483df4c042013125
Tom Qiu <tomqiu@Toms-MacBook-Pro-15.local>
2020-05-16 22:53:55 -0700

    Make generator work

--- api/antonym_generator.py
+++ api/antonym_generator.py
@@ -10,27 +10,31 @@ class AntonymGenerator:
 
     def process_string(self, paragraph):
         output = []
-        split_string = nltk.word_tokenize(m)
+        split_string = nltk.word_tokenize(paragraph)
         for word in split_string:
+            antonym_found = False
             if word not in self.punctuation:
-                syns = wn.synsets("program")
-                antonym_found = False
-                if syns[0].pos() == 'a':
+                syns = wn.synsets(word)
+
+                if syns and syns[0].pos() in ['a', 's', 'n']:
                     for synset in syns:
-                        if not antonym_found:
+                        if antonym_found:
                             break
                         for lemma in synset.lemmas():
+                            # print(word, lemma)
                             if lemma.antonyms():
                                 output.append(lemma.antonyms()[0].name())
                                 antonym_found = True
                                 break
-                if not antonym_found:
-                    break
+                if antonym_found:
+                    continue
             output.append(word)
-        return output
+
+        return " ".join(output)
 
 
-newGenerator = AntonymGenerator()
-paragraph1 = "Apple is looking at buying U.K. startup for $1 billion. Is that tiny Rick?"
-paragraph1 = "Apple is looking at buying U.K. startup for $1 billion. \n Is that tiny Rick?"
-print(newGenerator.process_string())
+# newGenerator = AntonymGenerator()
+# paragraph1 = "Apple is looking at buying U.K. startup for $1 billion. That is a big elephant."
+# paragraph2 = "You are a bad person, Delly."
+# print(newGenerator.process_string(paragraph1))
+# print(newGenerator.process_string(paragraph2))
--- api/api.py
+++ api/api.py
@@ -1,20 +1,21 @@
 import time
 from flask import Flask, request
 
+from antonym_generator import AntonymGenerator
+
 app = Flask(__name__)
+antonym_generator = AntonymGenerator()
+
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
 
+
 @app.route('/magicwords', methods=['POST'])
 def get_magic_words():
-  data = request.get_json()
-  value = data['value']
+    data = request.get_json()
+    value = data['value']
 
-  # TODO: insert logic here
-  print(value)
-
-  # TODO: Input return value here
-  magic_string = '123123'
-  return {'data': magic_string}
+    ouput_string = antonym_generator.process_string(value)
+    return {'data': ouput_string}
